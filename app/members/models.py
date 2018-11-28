2aee85e649c8c1c662fcfe94d59b93674e0faa90
ElonMoon <elonmoon@gmail.com>
2018-11-28 13:00:45 +0900

    User model 추가 및 aws키 주석

--- app/members/models.py
+++ app/members/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     img_profile = models.ImageField(upload_to='user', blank=True)
+    phone = models.CharField(max_length=13)
+    is_host = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
+
