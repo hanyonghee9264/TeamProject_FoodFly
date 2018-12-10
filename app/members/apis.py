61ddd1b2125f71ef9a65560486fcf2de058d4baa
ElonMoon <elonmoon@gmail.com>
2018-12-10 19:46:44 +0900

    FacebookLogin 완료

--- app/members/apis.py
+++ app/members/apis.py
@@ -4,7 +4,7 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import AuthTokenSerializer, UserRegisterSerializer, UserSerializer
+from .serializers import AuthTokenSerializer, UserRegisterSerializer, UserSerializer, FacebookSerializer
 
 User = get_user_model()
 
@@ -43,3 +43,12 @@ class Profile(APIView):
         user = request.user
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class FacebookAuthToken(APIView):
+    def post(self, request):
+        serializer = FacebookSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
--- app/members/urls.py
+++ app/members/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from members.apis import AuthToken, UserRegister, Profile
+from members.apis import AuthToken, UserRegister, Profile, FacebookAuthToken
 
 urlpatterns = [
     path('login/', AuthToken.as_view()),
     path('register/', UserRegister().as_view()),
     path('profile/', Profile.as_view()),
+    path('facebook/', FacebookAuthToken.as_view()),
 ]
