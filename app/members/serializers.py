366b734c2a0b3f475d4b2f23d72f3966740b8fde
ElonMoon <elonmoon@gmail.com>
2018-12-11 17:56:50 +0900

    Address detail field 수정

--- app/members/serializers.py
+++ app/members/serializers.py
@@ -3,9 +3,8 @@ from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import AuthenticationFailed
 
+
 User = get_user_model()
-UNUSABLE_PASSWORD_PREFIX = '!'  # This will never be a valid encoded hash
-UNUSABLE_PASSWORD_SUFFIX_LENGTH = 40  # number of random chars to add after UNUSABLE_PASSWORD_PREFIX
 
 
 class UserSerializer(serializers.ModelSerializer):
