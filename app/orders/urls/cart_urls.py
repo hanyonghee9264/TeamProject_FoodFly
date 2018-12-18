194b821f299e0e0e59c4dce9e0823756325854d2
ElonMoon <elonmoon@gmail.com>
2018-12-18 16:07:47 +0900

    CartItem APIView, Serializer 수정

--- app/orders/urls/cart_urls.py
+++ app/orders/urls/cart_urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from ..apis import CartItemList
+from ..apis import CartItemList, CartItemDetail
 
 urlpatterns = [
     path('items/', CartItemList.as_view()),
+    path('items/<int:pk>/', CartItemDetail.as_view()),
 ]
